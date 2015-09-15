5cbe29e957879cbcdda5beb3c2d65d4763a91141
nazmulhasan <nazmul@apurbatech.com>
2015-09-15 13:13:24 +0600

    login page is integrated

--- application/views/member/login.php
+++ application/views/member/login.php
@@ -25,7 +25,7 @@
                 </div>
             </div>
             <div class="col-md-5">
-                <?php echo form_open("auth/login"); ?>
+                
                 <?php if (isset($message) && ($message != NULL)): ?>
                     <div class="alert alert-dismissible" style="background-color: #703684;color: #ffffff"><?php echo $message; ?></div>
                 <?php endif; ?>
@@ -101,54 +101,12 @@
                 </div>
                 <div class="row form-group">
                     <div class="col-md-12 col-sm-12 col-xs-12">
-                        <?php echo form_input($register_btn + array('class' => 'btn button-custom', 'style' => 'background-color: #703684; color: white')); ?>
-                        <!--<button id="btnSubmit" type="submit" class="btn button-custom" style="background-color: #703684; color: white"><b>Sign Up</b></button>-->
+                        <?php echo form_input($register_btn + array('class' => 'btn button-custom', 'style' => 'background-color: #703684; color: white')); ?>                        
                     </div>
-                </div>
-                <?php echo form_close(); ?>
+                </div>                
             </div>
             <div class="col-md-1"></div>
         </div>
-        
-        
-<!--        <div class="row form-group">
-            <div class="col-md-1" style="padding-left: 3px; padding-right: 3px; "></div>
-            <?php //for ($i = 0; $i < 10; $i++) { ?>
-                <div class="col-md-1 col-sm-2 col-xs-6 form-group user_brief_card" style="padding-left: 3px; padding-right: 3px; cursor: pointer;">
-                    <div id="brand" class="brand_single_image">
-                        <div class="brand_cover_single_image">
-                            <img height="45" width="45" class="img_pad_mar_top_single_image" src="<?php echo base_url(); ?>resources/images/face.jpg"><br>
-                            <p class="font_10px" style="margin-right: 8px;">Nazrul Islam</p>
-                            <img style="margin-top: -12px;" height="22" width="45" src="<?php echo base_url(); ?>resources/images/flag.png"><br>
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-12">
-                                <span class="label_padding_top_single_image" >Nazrul Islam</span>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-offset-1 col-md-10">
-                                <ul style="margin: 0px; padding: 0px; padding-top: 5px; list-style-type: none;">
-                                    <li style="padding: 2px 0px;" class="font_10px">Profession:
-                                        <ul style="margin: 0px; padding: 0px; list-style-type: none;">
-                                            <li class="info_style_single_image">Doctor</li>
-                                        </ul>
-                                    </li>
-                                    <li style="padding: 2px 0px;" class="font_10px">Age:
-                                        <ul style="margin: 0px; padding: 0px; list-style-type: none;">
-                                            <li class="info_style_single_image ">30 Years</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="col-md-offset-1"></div>
-                        </div>
-                    </div>
-                </div>
-            <?php //} ?>
-            <div class="col-md-1" style="padding-left: 3px; padding-right: 3px; "></div>
-        </div>-->
         
         <div class="row form-group">
             <div class="col-md-1" style="padding-left: 3px; padding-right: 3px; "></div>
